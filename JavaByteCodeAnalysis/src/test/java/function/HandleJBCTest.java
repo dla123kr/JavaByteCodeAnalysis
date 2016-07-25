3f543803dfd0d3d064a77c6d6664c74308b380e5
Walter <walter@jennifersoft.com>
2016-07-25 17:45:19 +0900

    JUnit을 이용한 테스트1

---
+++ JavaByteCodeAnalysis/src/test/java/function/HandleJBCTest.java
@@ -0,0 +1,145 @@
+package function;
+
+import javassist.bytecode.ClassFile;
+import model.JBCClass;
+import model.Node;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Created by LimSJ on 2016-07-25.
+ */
+public class HandleJBCTest {
+
+    private int cnt = 0;
+    private String str;
+
+    /**
+     * 해당 파일을 테스트 (*.jar)
+     *
+     * @throws Exception
+     */
+    @Test
+    public void addNodeTest() throws Exception {
+        HandleJBC.getStaticNodes().clear();
+        HandleJBC.getStaticNodes().add(new Node("(default)"));
+
+        BufferedInputStream bis = null;
+        ClassFile classFile = null;
+
+        bis = new BufferedInputStream(new FileInputStream("D:\\[자료]\\jennifer\\jennifer-server-5.1.0.5\\jennifer-data-server-5.1.0.5\\server.data\\lib\\com.jennifersoft.data-5.1.0.5.jar")); // TODO: 2016-07-25 변경
+        ZipInputStream zis = new ZipInputStream(bis);
+        for (ZipEntry entry = zis.getNextEntry(); entry != null; entry = zis.getNextEntry()) {
+            if (!entry.isDirectory() && entry.getName().toLowerCase().endsWith(".class")) {
+                String className = entry.getName().replace('/', '.');
+                classFile = new ClassFile(new DataInputStream(zis));
+                HandleJBC.addNode(classFile);
+            }
+        }
+        HandleJBC.countingMethodCall();
+
+        // 테스트
+        classFileCountTest("com", new String[]{"jennifersoft", "data", "command", "agentinfo"}, 15);
+        classFileCountTest("com", new String[]{"jennifersoft", "data", "db", "migration"}, 10);
+        classFileCountTest("com", new String[]{"jennifersoft", "e"}, 7);
+        classFileCountTest("com", new String[]{"jennifersoft", "data", "useragent", "parser"}, 34);
+        classFileCountTest("com", new String[]{"jennifersoft", "data"}, 66);
+        classFileCountTest("org", new String[]{"json"}, 20);
+        classFileCountTest("jennifer", new String[]{"annotaion"}, 2);
+        classFileCountTest("jennifer", new String[]{"core"}, 35);
+        classFileCountTest("jennifer", new String[]{"core", "field"}, 29);
+        classFileCountTest("jennifer", new String[]{"lang", "util"}, 1);
+        classFileCountTest("jennifer", new String[]{"lang", "record"}, 44);
+        classFileCountTest("jennifer", new String[]{"version"}, 4);
+        classFileCountTest("jennifer", new String[]{"net", "packet"}, 3);
+
+        checkParentTest("com", new String[]{"jennifersoft"}, "com");
+        checkParentTest("com", new String[]{"jennifersoft", "b"}, "jennifersoft");
+        checkParentTest("com", new String[]{"jennifersoft", "data", "cache"}, "data");
+        checkParentTest("com", new String[]{"jennifersoft", "data", "cache", "CachedLong"}, "cache");
+        checkParentTest("org", new String[]{"json"}, "org");
+        checkParentTest("org", new String[]{"json", "JSONString"}, "json");
+    }
+
+    /**
+     * 해당 경로의 실제로 존재하는(isLoaded) 클래스 파일의 수를 셈
+     *
+     * @param start    시작 경로
+     * @param paths    나머지 경로
+     * @param expected 기대값
+     */
+    private void classFileCountTest(String start, String[] paths, int expected) {
+        cnt = 0;
+        ArrayList<String> packs = new ArrayList<>(Arrays.asList(paths));
+        for (int i = 1; i < HandleJBC.getStaticNodes().size(); i++) {
+            if (HandleJBC.getStaticNodes().get(i).getName().equals(start)) {
+                recursiveClassFileCount(HandleJBC.getStaticNodes().get(i), packs);
+                break;
+            }
+        }
+        Assert.assertEquals(expected, cnt);
+    }
+
+    private void recursiveClassFileCount(Node node, ArrayList<String> packs) {
+        if (packs.size() == 0) {
+            for (int i = 0; i < node.getClasses().size(); i++) {
+                if (((JBCClass) node.getClasses().get(i)).getIsLoaded())
+                    cnt++;
+            }
+        } else {
+            for (int i = 0; i < node.getPackages().size(); i++) {
+                if (node.getPackages().get(i).getName().equals(packs.get(0))) {
+                    packs.remove(0);
+                    recursiveClassFileCount(node.getPackages().get(i), packs);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 해당 경로의 부모이름을 테스트
+     *
+     * @param start    시작 경로
+     * @param paths    나머지 경로
+     * @param expected 기대값
+     */
+    private void checkParentTest(String start, String[] paths, String expected) {
+        str = "";
+        ArrayList<String> packs = new ArrayList<>(Arrays.asList(paths));
+        for (int i = 1; i < HandleJBC.getStaticNodes().size(); i++) {
+            if (HandleJBC.getStaticNodes().get(i).getName().equals(start)) {
+                recursiveCheckParent(HandleJBC.getStaticNodes().get(i), packs);
+                break;
+            }
+        }
+        Assert.assertEquals(expected, str);
+    }
+
+    private void recursiveCheckParent(Node node, ArrayList<String> packs) {
+        if (packs.size() == 1) {
+            // 클래스이름이나 패키지이름
+            for (int i = 0; i < node.getChildren().size(); i++) {
+                if (node.getChildren().get(i).getName().equals(packs.get(0))) {
+                    str = node.getChildren().get(i).getParentName();
+                }
+            }
+        } else {
+            for (int i = 0; i < node.getPackages().size(); i++) {
+                if (node.getPackages().get(i).getName().equals(packs.get(0))) {
+                    packs.remove(0);
+                    recursiveCheckParent(node.getPackages().get(i), packs);
+                    break;
+                }
+            }
+        }
+    }
+}
